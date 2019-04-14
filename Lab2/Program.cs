fbe0314b48c241639d1cf92dfaa0bbb50b078164
gene <gdrobinson001@gmail.com>
2019-04-13 20:36:02 -0400

    Lab 2

---
+++ Lab2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                {
+                    Console.WriteLine("Welcome to Grand Circus' Room Detail Generator!\n");
+                    while (true)
+                    {
+                        Console.WriteLine();
+                        Console.Write("Enter Length: ");
+                        decimal length = decimal.Parse(Console.ReadLine());
+                        Console.Write("Enter Width: ");
+                        decimal width = decimal.Parse(Console.ReadLine());
+
+                        decimal area = length * width;
+                        decimal perimeter = 2 * (length * width);
+
+                        Console.WriteLine("Area: " + area);
+                        Console.WriteLine("Perimeter: " + perimeter);
+                        Console.WriteLine();
+                    
+
+                        while (true)
+                        {
+                            Console.WriteLine();
+                            Console.Write("Continue? (y/n): ");
+                            string answer = Console.ReadLine();
+                            if (answer == "y")
+                            break;
+                            if (answer == "n")
+                            return;
+                            Console.ReadKey();
+                        }
+
+                    }
+                    
+                }  
+                
+                
+            }
+            
+        }
+    }
+}
